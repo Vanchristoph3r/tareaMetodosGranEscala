6f3e9a0bf0936e01b022016c0bd8c0fdd9e4d1ac
christopher <christopher@chrismbp.local>
2023-02-15 17:58:22 -0600

    tarea05: agregar argumentos al comando

--- README.md
+++ README.md
@@ -5,17 +5,24 @@ Traslado de jupyter notebook a un script modular
 ## Instalación
 Debe tener [pipenv](https://pipenv.pypa.io/en/latest/install/#installing-pipenv) instalado y activar el ambiente virtual.
 
+Para instalar pipenv solo basta con
+```bash
+pip3 install pipenv
+```
+Después instalar y activar el ambiente
 ```bash 
-pipenv shell
 pipenv install
+pipenv shell
 ```
 
 ## Uso
 Con el ambiente activado será suficiente ejecutar el siguiente comando.
 
 ```bash
-python run.py
+python run.py [-f your_config_file.yaml]
 ```
+
+Por default tiene un archivo de configuración pero puedes agregar el tuyo en un argumento.
 
 ## Estructura
 - data: contiene los datos *raw* y *outputs*
--- run.py
+++ run.py
@@ -1,23 +1,38 @@
 """Main executor"""
+import argparse
 import logging
 import sys
 import yaml
 
+
 from src.estimation import get_estimation
+
+parser = argparse.ArgumentParser(
+    prog="Trainmodel", 
+    usage="use: %(prog)s [-f] file_to_config",
+    description="Trains house model prediction")
+parser.add_argument("-f", "--file", type=str)
+args = parser.parse_args()
 
 logging.basicConfig(level=logging.DEBUG, stream=sys.stdout)
 
 
-def load_config() -> dict:
+def load_config(file_path: str) -> dict:
     """Loads yaml config
 
     Returns:
         dict: dict of config vars
     """
-    with open("config.yaml", "r") as file:
+    with open(file_path, "r") as file:
         config = yaml.safe_load(file)
     return config
 
 
 if __name__ == "__main__":
-    get_estimation(load_config())
+    if args.file:
+        file_path = args.file
+        logging.info("File path args %s", file_path)
+    else:
+        file_path = "config.yaml"
+
+    get_estimation(load_config(file_path))
